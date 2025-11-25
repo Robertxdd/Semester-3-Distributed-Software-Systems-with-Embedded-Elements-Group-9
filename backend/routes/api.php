294e835f6ca7f7b013872a06116a850a17011f9b
Robert <robert820404@gmail.com>
2025-11-25 19:33:04 +0100

    yes

--- backend/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DeskController;
-
-Route::get('/desks', [DeskController::class, 'getAll']);
-Route::get('/desks/{id}', [DeskController::class, 'getOne']);
-Route::put('/desks/{id}/state', [DeskController::class, 'updateState']);
--- backend/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return ['status' => 'ok'];
-});  // to start php artisan serve
